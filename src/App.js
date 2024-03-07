b7c4a8d0aa22878c137cf06564d239f3192184fd
Jason Rango <kcrango@hotmail.com>
2024-03-07 15:53:55 -0600

    lint and side nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Toaster from 'react-hot-toast';
+import SideNav from './components/layout/SideNav';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <SideNav />
+
+        <Toaster position='top-right' reverseOrder={false} toastOptions={{
+          duration: 3000,
+          success: {
+            style: {
+              borderLeftColor: '#3daf8d'
+            },
+            iconTheme: {
+              primary: '#3daf8d'
+            }
+          },
+          error: {
+            duration: 5000,
+            iconTheme: {
+              primary: '#9e0442'
+            }
+          }
+        }} />
+      </>
   );
-}
+};
 
 export default App;
